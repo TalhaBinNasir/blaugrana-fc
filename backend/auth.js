b5f68475eba7d521ea50f628a31351e37a4bd7f1
Talha Bin Nasir <tnasir@ssidecisions.com>
2024-05-21 16:52:46 +0500

    jwt token integration

---
+++ backend/auth.js
@@ -0,0 +1,43 @@
+const jwt = require("jsonwebtoken");
+
+function generateAccessToken(user) {
+  const payload = {
+    id: user.id,
+    username: user.username,
+  };
+
+  const secret = process.env.SECRET_ACCESS_TOKEN;
+  const options = { expiresIn: "1h" };
+
+  return jwt.sign(payload, secret, options);
+}
+
+function verifyAccessToken(token) {
+  const secret = process.env.SECRET_ACCESS_TOKEN;
+
+  try {
+    const decoded = jwt.verify(token, secret);
+    return { success: true, data: decoded };
+  } catch (error) {
+    return { success: false, error: error.message };
+  }
+}
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) {
+    return res.sendStatus(401);
+  }
+
+  const result = verifyAccessToken(token);
+
+  if (!result.success) {
+    return res.status(403).json({ error: result.error });
+  }
+
+  req.user = result.data;
+  next();
+}
+module.exports = { generateAccessToken, verifyAccessToken, authenticateToken };
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,36 @@
 const express = require("express");
+const dotenv = require("dotenv").config();
+const bodyParser = require("body-parser");
 const app = express();
 
-const PORT = 3000;
+const { generateAccessToken, authenticateToken } = require("./auth.js");
 
-app.listen(PORT, () => {
-  console.log(`Server started on PORT: ${PORT}`);
+app.use(bodyParser.json());
+
+const data = [
+  { id: 1, username: "talhabinnasir" },
+  { id: 2, username: "johncena" },
+];
+
+app.post("/login", (req, res) => {
+  const { username } = req.body;
+  const user = data.find((user) => user.username === username);
+  if (user) {
+    const token = generateAccessToken(req.body);
+    res
+      .status(200)
+      .json({ message: `${username} successfully logged in!, Toke: ${token}` });
+  } else {
+    res.status(401).json({ message: `Error! Please try again.` });
+  }
+});
+
+app.get("/protected", authenticateToken, (req, res) => {
+  res
+    .status(200)
+    .json({ message: "You have accessed a protected route!", user: req.user });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server started on PORT: ${process.env.PORT}`);
 });
